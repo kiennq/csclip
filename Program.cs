61b3873c09993ea923c1052ffc83872e461b8e7a
Kien Nguyen <kien.n.quang@gmail.com>
2019-02-25 19:07:23 +0900

    add nuget packages

--- Program.cs
+++ Program.cs
@@ -2,14 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace csclip
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Task.Run(() =>
+            {
+                // Message pump
+                Application.Run();
+            });
+
+            var wg = new Barrier(1);
+            var program = Program.CreateInstance();
+
+            if (args.Length > 0)
+            {
+                Console.WriteLine(program.ProcessRequestAsync(args).Result);
+            }
+            else
+            {
+                string s;
+                while ((s = Console.ReadLine()) != null)
+                {
+                    Console.WriteLine(program.ProcessRequestAsync(s.Split()).Result);
+                }
+            }
+
+            Application.Exit();
+        }
+
+        static Program CreateInstance()
+        {
+            return new Program();
+        }
+
+        async Task<string> ProcessRequestAsync(string[] args)
+        {
+            return await Task.FromResult<string>(string.Join("+", args));
         }
     }
 }

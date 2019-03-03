fb4fcf36a98e757c3d419cad9c57cdc2f11e27d8
Kien Nguyen <kien.n.quang@gmail.com>
2019-03-04 04:59:21 +0900

    refactoring test

--- csclip.test/ProgramTests.cs
+++ csclip.test/ProgramTests.cs
@@ -16,34 +16,55 @@ namespace csclip.test
             {
                 var input = new MemoryStream();
                 Console.SetIn(new StreamReader(input));
-                Stdin = new StreamWriter(input);
+                Stdin = new StreamWriter(input) { AutoFlush = true };
 
                 var output = new MemoryStream();
-                Console.SetOut(new StreamWriter(output));
+                Console.SetOut(new StreamWriter(output) {AutoFlush = true });
                 Stdout = new StreamReader(output);
             }
+        }
+
+        void KeepPositionInvoke(Stream stream, Action action)
+        {
+            var pos = stream.Position;
+            action();
+            stream.Position = pos;
         }
 
         [TestMethod]
         public void TestCopyAndPaste()
         {
             var program = new Program();
-            var console = new MockConsole();
+            var con = new MockConsole();
 
             // Copy
             var input = "Test";
-            console.Stdin.Write(input);
+            KeepPositionInvoke(con.Stdin.BaseStream, () =>
+            {
+                con.Stdin.Write(input);
+            });
             program.Run(new string[] { "copy" });
 
             // Paste
-            program.Run(new string[] { "paste" });
-            Assert.AreEqual(console.Stdout.ReadToEnd(), input);
+            KeepPositionInvoke(con.Stdout.BaseStream, () =>
+            {
+                program.Run(new string[] { "paste" });
+            });
+            Assert.AreEqual(con.Stdout.ReadToEnd(), input);
+
+            // Paste
+            KeepPositionInvoke(con.Stdout.BaseStream, () =>
+            {
+                program.Run(new string[] { "paste", "--rpc-format" });
+            });
+            Assert.AreEqual(con.Stdout.ReadToEnd(), String.Format("15\r\n{{\"data\":\"{0}\"}}", input));
         }
 
         [TestMethod]
         public void TestCopyAndPasteMultiFormats()
         {
             var program = new Program();
+            var con = new MockConsole();
 
             // Copy
             var inputText = "Test";
@@ -55,22 +76,34 @@ StartFragment:00000153
 EndFragment:00000164
 <!DOCTYPE><HTML><HEAD></HEAD><BODY><!--StartFragment --><b>Test</b><!--EndFragment --></BODY></HTML>";
 
-            Console.SetIn(new StringReader("[{\"cf\":\"text\", \"data\":\"" + inputText + "\"}, {\"cf\":\"html\", \"data\":\"" + inputHtml + "\"}]"));
+            KeepPositionInvoke(con.Stdin.BaseStream, () =>
+            {
+                con.Stdin.Write(String.Format("[{{\"cf\":\"text\", \"data\":\"{0}\"}}, {{\"cf\":\"html\", \"data\":\"{1}\"}}]", inputText, inputHtml));
+            });
             program.Run(new string[] { "copy" });
 
             // Paste
             {
-                var stdout = new StringWriter();
-                Console.SetOut(stdout);
-                program.Run(new string[] { "paste" });
-                Assert.AreEqual(stdout.ToString(), inputText);
+                KeepPositionInvoke(con.Stdout.BaseStream, () =>
+                {
+                    program.Run(new string[] { "paste" });
+                });
+                Assert.AreEqual(con.Stdout.ReadToEnd(), inputText);
             }
             {
-                var stdout = new StringWriter();
-                Console.SetOut(stdout);
-                program.Run(new string[] { "paste", "-f", "html" });
-                Assert.AreEqual(stdout.ToString(), outputHtml);
+                KeepPositionInvoke(con.Stdout.BaseStream, () =>
+                {
+                    program.Run(new string[] { "paste", "-f", "html" });
+                });
+                Assert.AreEqual(con.Stdout.ReadToEnd(), outputHtml);
             }
+        }
+
+        [TestMethod]
+        public void TestServer()
+        {
+            var program = new Program();
+            var con = new MockConsole();
         }
     }
 }
